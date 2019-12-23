ace7b18fd8b1e6c0ba29b8c6469b5cf244b1d6c4
deadplut <suganoff@yandex.ru>
2019-12-23 17:59:27 +0300

    1.0.2
    Конец 3-го, начало 4-го

--- Game.py
+++ Game.py
@@ -5,26 +5,42 @@ win = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("Cubes Game")
 x = 50
-y = 50
+y = 425
 widht = 40
 height = 60
 speed = 5
-
+isJump = False
+jumpCount = 10
 run=True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x>5:
         x-=speed
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x<500-widht -5 :
         x += speed
-    if keys[pygame.K_UP]:
-        y-= speed
-    if keys[pygame.K_DOWN]:
-        y+=speed
+    if not(isJump):
+        if keys[pygame.K_UP] and y>5:
+            y-= speed
+        if keys[pygame.K_DOWN] and y<500-height-15:
+            y+=speed
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            if jumpCount < 0:
+                y += (jumpCount ** 2) / 2
+            else:
+                y -= (jumpCount ** 2)/2
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+
     win.fill((0,0,0))
     pygame.draw.rect(win, (0,0,255), (x, y , widht, height))
     pygame.display.update()
